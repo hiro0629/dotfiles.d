6bf4e4ae7b1382f95d034f6e4a3578de1424aa9e
Tomohisa Hirota <hirota.tomohisa@gmail.com>
2025-03-09 05:32:12 +0900

    added inputrc to install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ ln -sf "$DOTFILES_DIR/bash/bash_profile" "$HOME/.bash_profile"
 # ln -sf "$DOTFILES_DIR/zsh/zshrc" "$HOME/.zshrc"
 # ln -sf "$DOTFILES_DIR/zsh/zsh_profile" "$HOME/.zsh_profile"
 ln -sf "$DOTFILES_DIR/nvim/init.lua" "$HOME/.config/nvim/init.lua"
+OS_TYPE=$(uname -s)
+if [[ "$OS_TYPE" == "Linux" ]]; then
+	ln -sf "$DOTFILES_DIR/inputrc" "$HOME/.inputrc"
+fi
 
 echo "Dotfiles installation complete!"
 

f82ff9bc459f9acef31c81c48f39f841d30005bf
Tomohisa Hirota <hirota.tomohisa@gmail.com>
2025-03-08 09:03:34 +0900

    changed dir structure
    bash dir and put into it of bash files

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DOTFILES_DIR="$HOME/new_dotfiles"
+
+# シンボリックリンク作成
+# ln -sf "$DOTFILES_DIR/bash/bashrc" "$HOME/.bashrc"
+ln -sf "$DOTFILES_DIR/bash/bash_profile" "$HOME/.bash_profile"
+# ln -sf "$DOTFILES_DIR/zsh/zshrc" "$HOME/.zshrc"
+# ln -sf "$DOTFILES_DIR/zsh/zsh_profile" "$HOME/.zsh_profile"
+
+echo "Dotfiles installation complete!"
+

4fb94730024e0bfd8e0c945e8a1a6a001af725d0
Tomohisa Hirota <hirota.tomohisa@gmail.com>
2025-03-09 06:13:59 +0900

    added mkdir local_config.d

--- install.sh
+++ install.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 DOTFILES_DIR="$HOME/dotfiles.d"
+OS_TYPE=$(uname -s)
 
 # シンボリックリンク作成
 ln -sf "$DOTFILES_DIR/bash/bashrc" "$HOME/.bashrc"
 ln -sf "$DOTFILES_DIR/bash/bash_profile" "$HOME/.bash_profile"
+if [[ ! -d "$HOME/local_config.d" ]]; then
+	mkdir ~/local_config.d
+	touch ~/local_config.d/alias
+fi
 # ln -sf "$DOTFILES_DIR/zsh/zshrc" "$HOME/.zshrc"
 # ln -sf "$DOTFILES_DIR/zsh/zsh_profile" "$HOME/.zsh_profile"
 ln -sf "$DOTFILES_DIR/nvim/init.lua" "$HOME/.config/nvim/init.lua"
-OS_TYPE=$(uname -s)
 if [[ "$OS_TYPE" == "Linux" ]]; then
 	ln -sf "$DOTFILES_DIR/inputrc" "$HOME/.inputrc"
 fi
 ln -sf "$DOTFILES_DIR/gitconfig" "$HOME/.gitconfig"
 
-
 echo "Dotfiles installation complete!"
-

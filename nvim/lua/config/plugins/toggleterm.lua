1cfdd1d65ab09b59e319bbbfc23afcd4c39f3b10
Tomohisa Hirota <hirota.tomohisa@gmail.com>
2025-03-13 09:16:43 +0900

    install toggleterm and related to lazygit

---
+++ nvim/lua/config/plugins/toggleterm.lua
@@ -0,0 +1,41 @@
+return {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+--        require("toggleterm").setup()
+        require("toggleterm").setup{
+            -- 必要に応じて設定を追加
+            open_mapping = [[<c-\>]], -- <C-\>でターミナルを開閉
+            direction = 'float',     -- 浮動ウィンドウで表示
+            -- その他の設定は公式ドキュメントを参照
+        }
+
+        -- Lazygit を Neovim のフローティングウィンドウで開く
+        local Terminal = require("toggleterm.terminal").Terminal
+        local lazygit = Terminal:new({
+            cmd = "lazygit",
+            direction = "float",
+            hidden = true
+        })
+
+        function _lazygit_toggle()
+            lazygit:toggle()
+        end
+
+        vim.api.nvim_set_keymap("n", "lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+    end
+}
+
+
+-- return {
+--   "akinsho/toggleterm.nvim",
+--   version = "*",
+--   config = function()
+--     require("toggleterm").setup{
+--       -- 必要に応じて設定を追加
+--       open_mapping = [[<c-\>]], -- <C-\>でターミナルを開閉
+--       direction = 'float',     -- 浮動ウィンドウで表示
+--       -- その他の設定は公式ドキュメントを参照
+--     }
+--   end
+-- }

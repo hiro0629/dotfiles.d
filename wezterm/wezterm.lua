28c7491519a9a1ef5708ee4e8d0340baebe0d2a3
Tomohisa Hirota <hirota.tomohisa@gmail.com>
2025-03-14 20:22:28 +0900

    Change appearance of wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,7 +10,7 @@ end
 -- フォントの設定
 config.font = wezterm.font("Meslo LG L for Powerline", {weight="Medium", stretch="Normal", style="Normal"})
 -- フォントサイズの設定
-config.font_size = 16
+config.font_size = 13
 
 -- カラースキームの設定
 -- config.color_scheme = 'AdventureTime'
@@ -19,6 +19,44 @@ config.font_size = 16
 config.color_scheme = 'nightfox'
 -- 背景透過
 config.window_background_opacity = 0.78
+
+config.macos_window_background_blur = 20
+config.window_decorations = "NONE"
+config.window_frame = {
+  inactive_titlebar_bg = "none",
+  active_titlebar_bg = "none",
+}
+ config.window_background_gradient = {
+   colors = { "#192330" },
+ }
+config.show_new_tab_button_in_tab_bar = false
+config.colors = {
+ tab_bar = {
+   inactive_tab_edge = "none",
+ },
+}
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+   local background = "#2f1922"
+   local foreground = "#5c6d74"
+
+   if tab.is_active then
+--     background = "e6dccf"
+--     background = "2f1922"
+     background = "232f19"
+     foreground = "#FFFFFF"
+   end
+
+   local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+
+   return {
+     { Background = { Color = background } },
+     { Foreground = { Color = foreground } },
+     { Text = title },
+   }
+end)
+
+
 
 -- Scroll back
 config.scrollback_lines = 3500

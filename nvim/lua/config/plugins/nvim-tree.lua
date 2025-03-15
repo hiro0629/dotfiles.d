663daa04bcf360ad98d92fc63180a0deb2c6e730
Tomohisa Hirota <hirota.tomohisa@gmail.com>
2025-03-15 16:08:59 +0900

    leader key

--- nvim/lua/config/plugins/nvim-tree.lua
+++ nvim/lua/config/plugins/nvim-tree.lua
@@ -1,4 +1,12 @@
  return {
-   "nvim-tree/nvim-tree.lua",
-   config = true,
-}
+  "nvim-tree/nvim-tree.lua",
+--  config = true,
+  config = function()
+    require("nvim-tree").setup({
+      view = {
+        width = 21, -- Set width of the directory tree
+        side = "left", -- Can also be "right"
+      },
+    })
+  end,
+} 
--- nvim/lua/config/plugins/toggleterm.lua
+++ nvim/lua/config/plugins/toggleterm.lua
@@ -5,9 +5,13 @@ return {
 --        require("toggleterm").setup()
         require("toggleterm").setup{
             -- 必要に応じて設定を追加
-            open_mapping = [[<c-\>]], -- <C-\>でターミナルを開閉
+--            open_mapping = [[<c-\>]], -- <C-\>でターミナルを開閉 default
+            open_mapping = "<space>t", -- <C-\>でターミナルを開閉
             direction = 'float',     -- 浮動ウィンドウで表示
             -- その他の設定は公式ドキュメントを参照
+            keymaps = {
+                ["<leader>t"] = "toggle"
+            },
         }
 
         -- Lazygit を Neovim のフローティングウィンドウで開く
@@ -22,7 +26,7 @@ return {
             lazygit:toggle()
         end
 
-        vim.api.nvim_set_keymap("n", "lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+        vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
     end
 }
 

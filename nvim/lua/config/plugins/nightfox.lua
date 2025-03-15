812992cae440ad3cbb51af3e6138b8c7919228f0
Tomohisa Hirota <hirota.tomohisa@gmail.com>
2025-03-15 18:56:37 +0900

    Opacity in neovim

--- nvim/lua/config/plugins/nightfox.lua
+++ nvim/lua/config/plugins/nightfox.lua
@@ -1,8 +1,14 @@
 return {
   "EdenEast/nightfox.nvim",
   config = function()
-    vim.cmd("colorscheme nightfox")
     vim.cmd("highlight! link WinSeparator GlyphPalette2")
     vim.cmd("highlight! Visual guibg=#4a3332")
+    vim.cmd("colorscheme duskfox")
+    require('nightfox').setup({
+      options = {
+        transparent = true,
+      },
+    })
+    vim.cmd("colorscheme duskfox")
   end,
 }

efbd27d7923c77dafd8bb96794072fcfd87372a6
Tomohisa Hirota <hirota.tomohisa@gmail.com>
2025-03-15 10:33:57 +0900

    edit noice popup

--- nvim/lua/config/plugins/noice.lua
+++ nvim/lua/config/plugins/noice.lua
@@ -11,5 +11,20 @@ return {
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
     "rcarriga/nvim-notify",
-    }
+    },
+  config = function()
+    require("noice").setup({
+      messages = {
+        enabled = true,
+        view = "mini", -- Use a small inline message instead of a floating window
+        timeout = 800, -- Shorter duration for messages
+      },
+      notify = {
+        enabled = true,
+        view = "mini", -- Use mini popup for notifications
+        timeout = 1800, -- Make popups disappear quickly
+      },
+    })
+  end,
 }
+
